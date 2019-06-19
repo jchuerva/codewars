5d69aa1b0e7d86f2a80a76293ef90825c594a185
Carlos Alarcon <jchuerva@github.com>
2019-06-19 10:54:21 +0200

    add convert a boolean to a string kata

---
+++ python/00_template.py
@@ -0,0 +1,8 @@
+# Convert a Boolean to a String
+# Level: 8kyu
+
+# In this programming exercise, you're going to learn about functions, boolean (true/false) values, strings, and the if-statement.
+
+
+def boolean_to_string(b):
+    return str(b)
